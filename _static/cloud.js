8a8aa0989dbffed3ba8b9b463b745df252cf65fa
Meghan McGarry <mbmcgarry@wisc.edu>
2015-09-23 13:12:50 -0500

    Generated master for commit 21a2f9f Author: Paul P.H. Wilson <wilsonp@engr.wisc.edu>
        Change pphw email and fix broken links.

--- _static/cloud.js
+++ _static/cloud.js
@@ -161,10 +161,7 @@ $(document).ready(function (){
 
   doc.append(holder);
 
-  if (typeof state === "undefined") {
-	var state = "uncollapsed";
-  };
-  if (state == "collapsed"){
+  if (typeof state === "undefined") {n	var state = "uncollapsed";n  };n  if (state == "collapsed"){
     doc.addClass("collapsed-sidebar");
     show_btn.show();
     hide_btn.hide();
